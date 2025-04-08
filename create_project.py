fc4490d12c964a29ab31fb3e04a9c2babf5385e6
Tuomas Lahtinen <tuomas123lahtinen@gmail.com>
2025-04-08 14:11:26 +0300

    Implement support for creating a project for submodule
    Update btstack:
      > Prepare to reuse template functionality from arbitrary location
      > Remove example/bin directory when deleting examples
      > Cleanup indents and multiline trailing spaces

---
+++ create_project.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+script_path = os.path.abspath(os.path.dirname(sys.argv[0]))
+project_path = os.path.join(script_path, sys.argv[1])
+btstack_root = os.path.join(script_path, 'btstack')
+
+sys.path.append(os.path.join(btstack_root, "port/max32630-fthr/scripts"))
+from create_project_makefile import createProjectMakefile
+
+createProjectMakefile(project_path, btstack_root)
