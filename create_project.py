205229b23dcec16a0d096ddd076bdfb35dc40bb5
Tuomas Lahtinen <tuomas123lahtinen@gmail.com>
2025-04-09 07:57:58 +0300

    Implement application submodule build
    Mimic the logic for example build.
    Fix project creation script output.
    Implement project .gitignore rules.

--- create_project.py
+++ create_project.py
@@ -8,5 +8,5 @@ btstack_root = os.path.join(script_path, 'btstack')
 
 sys.path.append(os.path.join(btstack_root, "port/max32630-fthr/scripts"))
 from create_project_makefile import createProjectMakefile
-
+print(f"Creating project in {project_path}")
 createProjectMakefile(project_path, btstack_root)
